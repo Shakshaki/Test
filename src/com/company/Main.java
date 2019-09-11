f43e979b2db85f6651c50fe2dd626f4a4fde71a9
iskineseren <s185157@student.dtu.dk>
2019-09-11 12:52:01 +0200

    ny version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-
+        System.out.println("yo yo angelo dude");
         for(int i = 0; i < 5; i++){
             System.out.println(i);
         }
