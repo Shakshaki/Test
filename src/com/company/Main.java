9acdaf5b0687f27f8e4686bca4ee2f2b0a65fb63
Shakshaki <55184920+Shakshaki@users.noreply.github.com>
2019-09-11 12:43:16 +0200

    Hej Iskineser

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
